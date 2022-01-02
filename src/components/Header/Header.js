6af7a5229795777cabedfaae984340513a63da66
Edward <edward.a.prado@gmail.com>
2022-01-02 18:11:46 -0500

    Switched from Bootstrap to MUI and updated affected components to use MUI.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,62 +1,359 @@
-import React from "react";
+import * as React from "react";
 import "./header.scss";
 
 import MonumentalIcon from "../../assets/branding/Icon_Monumental.svg";
-import MythgardIcon from "../../assets/branding/Icon_Mythgard.png";
+import MythgardIcon from "../../assets/branding/Icon_Mythgard-transparent.png";
 
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MenuLink from "@mui/material/Link";
+
+const companyPages = [
+	{
+		name: "Team",
+		link: "https://monumental.io/team/",
+	},
+	{
+		name: "Contact",
+		link: "https://monumental.io/contact/",
+	},
+	{
+		name: "Careers",
+		link: "https://monumental.catsone.com/careers/",
+	},
+];
+const gamePages = [
+	{
+		name: "Home",
+		link: "/",
+	},
+	{
+		name: "News",
+		link: "/news",
+	},
+	{
+		name: "Support",
+		link: "",
+	},
+];
 
 const Header = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElNavGames, setAnchorElNavGames] = React.useState(null);
+	const [anchorElNavDropdownMenu, setAnchorElNavDropdownMenu] = React.useState(null);
+	const open = Boolean(anchorElNavDropdownMenu);
+
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleOpenNavMenuGames = (event) => {
+		setAnchorElNavGames(event.currentTarget);
+	};
+
+	const handleCloseNavMenuGames = () => {
+		setAnchorElNavGames(null);
+	};
+
+	const handleNavDropdownMenuClick = (event) => {
+		setAnchorElNavDropdownMenu(event.currentTarget);
+	};
+
+	const handleNavDropdownMenuClose = (event) => {
+		setAnchorElNavDropdownMenu(null);
+	};
+
 	return (
 		<div>
-			<Navbar bg="dark" expand="lg" className="myth-nav myth-nav-company">
-				<Container>
-					<Navbar.Brand href="https://monumental.io/">
-						<img src={MonumentalIcon} className="myth-nav_logo" alt="Monumental Studio Icon."></img>
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-							<NavDropdown title="Games" id="basic-nav-dropdown">
-								<NavDropdown.Item href="#">Mythgard</NavDropdown.Item>
-								<NavDropdown.Item href="https://monumental.io/games/storm-wars/">Storm Wars</NavDropdown.Item>
-								<NavDropdown.Item href="https://monumental.io/games/looty-dungeon/">Looty Dungeon</NavDropdown.Item>
-								<NavDropdown.Item href="https://monumental.io/games/little-alchemist/">
-									Little Alchemist
-								</NavDropdown.Item>
-							</NavDropdown>
-							<Nav.Link href="https://monumental.io/team/">Team</Nav.Link>
-							<Nav.Link href="https://monumental.io/contact/">Contact</Nav.Link>
-							<Nav.Link href="https://monumental.catsone.com/careers/">Careers</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
+			<AppBar position="static" className="myth-nav-company-wrapper">
+				<Container className="myth-nav myth-nav-company">
+					<Toolbar disableGutters>
+						<Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+							<img src={MonumentalIcon} className="myth-nav_logo" alt="Monumental Studio Icon."></img>
+						</Typography>
+
+						<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<IconButton
+								size="large"
+								aria-label="account of current user"
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								onClick={handleOpenNavMenu}
+								color="inherit"
+							>
+								<MenuIcon />
+							</IconButton>
+							<Menu
+								id="menu-appbar"
+								anchorEl={anchorElNav}
+								anchorOrigin={{
+									vertical: "bottom",
+									horizontal: "left",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "left",
+								}}
+								open={Boolean(anchorElNav)}
+								onClose={handleCloseNavMenu}
+								sx={{
+									display: { xs: "block", md: "none" },
+								}}
+							>
+								<Button
+									id="games-dropdown"
+									aria-controls={open ? "games-dropdown_menu" : undefined}
+									aria-haspopup="true"
+									aria-expanded={open ? "true" : undefined}
+									onClick={handleNavDropdownMenuClick}
+									endIcon={<KeyboardArrowDownIcon />}
+								>
+									Games
+								</Button>
+								<Menu
+									id="company-dropdown_menu"
+									aria-labelledby="company-dropdown"
+									anchorEl={anchorElNavDropdownMenu}
+									open={open}
+									onClose={handleNavDropdownMenuClose}
+									anchorOrigin={{
+										vertical: "top",
+										horizontal: "left",
+									}}
+									transformOrigin={{
+										vertical: "top",
+										horizontal: "left",
+									}}
+								>
+									<MenuItem>
+										<Link
+											to={{
+												pathname: "/",
+											}}
+										>
+											Mythgard
+										</Link>
+									</MenuItem>
+									<MenuItem>
+										<a href="https://monumental.io/games/storm-wars/">Storm Wars</a>
+									</MenuItem>
+									<MenuItem>
+										<a href="https://monumental.io/games/looty-dungeon/">Looty Dungeon</a>
+									</MenuItem>
+									<MenuItem>
+										<a href="https://monumental.io/games/little-alchemist/">Little Alchemist</a>
+									</MenuItem>
+								</Menu>
+								{companyPages.map((page) => (
+									<MenuItem key={page.name} onClick={handleCloseNavMenu}>
+										<MenuLink
+											href={page.link}
+											underline="hover"
+											sx={{ color: "#fff", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+										>
+											{page.name}
+										</MenuLink>
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
+						<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<img src={MonumentalIcon} className="myth-nav_logo" alt="Monumental Studio Icon."></img>
+						</Typography>
+						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+							<Button
+								id="games-dropdown"
+								aria-controls={open ? "games-dropdown_menu" : undefined}
+								aria-haspopup="true"
+								aria-expanded={open ? "true" : undefined}
+								onClick={handleNavDropdownMenuClick}
+								endIcon={<KeyboardArrowDownIcon />}
+								sx={{ color: "#fff", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+							>
+								Games
+							</Button>
+							<Menu
+								id="company-dropdown_menu"
+								aria-labelledby="company-dropdown"
+								anchorEl={anchorElNavDropdownMenu}
+								open={open}
+								onClose={handleNavDropdownMenuClose}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "left",
+								}}
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "left",
+								}}
+								sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+							>
+								{/* <MenuItem>
+									<MenuLink
+										href="https://monumental.io/games/mythgard/"
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										Mythgard
+									</MenuLink>
+								</MenuItem>
+								<MenuItem>
+									<MenuLink
+										href="https://monumental.io/games/storm-wars/"
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										Storm Wars
+									</MenuLink>
+								</MenuItem>
+								<MenuItem>
+									<MenuLink
+										href="https://monumental.io/games/looty-dungeon/"
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										Looty Dungeon
+									</MenuLink>
+								</MenuItem>
+								<MenuItem>
+									<MenuLink
+										href="https://monumental.io/games/little-alchemist/"
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										Little Alchemist
+									</MenuLink>
+								</MenuItem> */}
+
+								<MenuItem>
+									<Link
+										to={{
+											pathname: "/",
+										}}
+									>
+										Mythgard
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<a href="https://monumental.io/games/storm-wars/">Storm Wars</a>
+								</MenuItem>
+								<MenuItem>
+									<a href="https://monumental.io/games/looty-dungeon/">Looty Dungeon</a>
+								</MenuItem>
+								<MenuItem>
+									<a href="https://monumental.io/games/little-alchemist/">Little Alchemist</a>
+								</MenuItem>
+							</Menu>
+							{companyPages.map((page) => (
+								<MenuItem key={page.name} onClick={handleCloseNavMenu}>
+									<MenuLink
+										href={page.link}
+										underline="hover"
+										sx={{ color: "#fff", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										{page.name}
+									</MenuLink>
+								</MenuItem>
+							))}
+						</Box>
+					</Toolbar>
 				</Container>
-			</Navbar>
-			<Navbar sticky="top" expand="lg" className="myth-nav myth-nav-game">
-				<Container>
-					<Navbar.Brand href="/">
-						<img src={MythgardIcon} className="myth-nav_logo"></img>
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-							<Nav.Link href="/">Home</Nav.Link>
-							<Nav.Link href="#link">News</Nav.Link>
-							<Nav.Link href="#link">Support</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-					<Navbar.Collapse className="justify-content-end">
-						<Navbar.Text>
-							<Nav.Link href="#link" class="btn-cta">
-								Play Free
-							</Nav.Link>
-						</Navbar.Text>
-					</Navbar.Collapse>
+			</AppBar>
+			<AppBar position="sticky">
+				<Container className="myth-nav myth-nav-game">
+					<Toolbar disableGutters>
+						<Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+							<img src={MythgardIcon} className="myth-nav_logo" alt="Mythgard Logo."></img>
+						</Typography>
+
+						<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<IconButton
+								size="large"
+								aria-label="account of current user"
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								onClick={handleOpenNavMenuGames}
+								color="inherit"
+							>
+								<MenuIcon />
+							</IconButton>
+							<Menu
+								id="menu-appbar"
+								anchorEl={anchorElNavGames}
+								anchorOrigin={{
+									vertical: "bottom",
+									horizontal: "left",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "left",
+								}}
+								open={Boolean(anchorElNavGames)}
+								onClose={handleCloseNavMenuGames}
+								sx={{
+									display: { xs: "block", md: "none" },
+								}}
+							>
+								{gamePages.map((page) => (
+									<MenuItem key={page.name} onClick={handleCloseNavMenuGames}>
+										<MenuLink
+											href={page.link}
+											underline="hover"
+											sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+										>
+											{page.name}
+										</MenuLink>
+									</MenuItem>
+								))}
+								<Button
+									key="PlayNow"
+									sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+								>
+									Play Now
+								</Button>
+							</Menu>
+						</Box>
+						<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<img src={MythgardIcon} className="myth-nav_logo" alt="Mythgard Logo."></img>{" "}
+						</Typography>
+						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+							{gamePages.map((page) => (
+								<MenuItem key={page.name} onClick={handleCloseNavMenu}>
+									<MenuLink
+										href={page.link}
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										{page.name}
+									</MenuLink>
+								</MenuItem>
+							))}
+							<Button
+								key="PlayNow"
+								sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+							>
+								Play Now
+							</Button>
+						</Box>
+					</Toolbar>
 				</Container>
-			</Navbar>
+			</AppBar>
 		</div>
 	);
 };
---
+++ src/components/HeaderGame/HeaderGame.js
@@ -0,0 +1,131 @@
+import * as React from "react";
+import "./headerGame.scss";
+
+import MythgardIcon from "../../assets/branding/Icon_Mythgard-transparent.png";
+
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import MenuLink from "@mui/material/Link";
+
+const gamePages = [
+	{
+		name: "Home",
+		link: "/",
+	},
+	{
+		name: "News",
+		link: "/news",
+	},
+	{
+		name: "Support",
+		link: "",
+	},
+];
+
+const HeaderGame = () => {
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElNavGames, setAnchorElNavGames] = React.useState(null);
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	const handleOpenNavMenuGames = (event) => {
+		setAnchorElNavGames(event.currentTarget);
+	};
+
+	const handleCloseNavMenuGames = () => {
+		setAnchorElNavGames(null);
+	};
+
+	return (
+		<AppBar position="sticky" class="myth-nav-game-wrapper">
+			<Container className="myth-nav myth-nav-game">
+				<Toolbar disableGutters>
+					<Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
+						<img src={MythgardIcon} className="myth-nav_logo" alt="Mythgard Logo."></img>
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenuGames}
+							color="inherit"
+							sx={{ position: "Absolute", top: "0", bottom: "0", right: "0" }}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNavGames}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNavGames)}
+							onClose={handleCloseNavMenuGames}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							{gamePages.map((page) => (
+								<MenuItem key={page.name} onClick={handleCloseNavMenuGames}>
+									<MenuLink
+										href={page.link}
+										underline="hover"
+										sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+									>
+										{page.name}
+									</MenuLink>
+								</MenuItem>
+							))}
+							<Button
+								key="PlayNow"
+								sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+							>
+								Play Now
+							</Button>
+						</Menu>
+					</Box>
+					<Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<img src={MythgardIcon} className="myth-nav_logo" alt="Mythgard Logo."></img>{" "}
+					</Typography>
+					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+						{gamePages.map((page) => (
+							<MenuItem key={page.name} onClick={handleCloseNavMenu}>
+								<MenuLink
+									href={page.link}
+									underline="hover"
+									sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}
+								>
+									{page.name}
+								</MenuLink>
+							</MenuItem>
+						))}
+						<Button key="PlayNow" sx={{ color: "#000", fontWeight: "bold", fontFamily: "Open Sans", fontSize: "14px" }}>
+							Play Now
+						</Button>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
+
+export default HeaderGame;
