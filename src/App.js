439550dc11d8be17b9e84011271bcbba7e1ae93f
Edward <edward.a.prado@gmail.com>
2022-03-07 14:32:27 -0500

    Fixed page routing issue.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import React from "react";
 
-import { HashRouter } from "react-router-dom";
-import { BrowserRouter as Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import News from "./pages/News/News";
 import NewsPost from "./pages/NewsPost/NewsPost";
@@ -11,7 +10,7 @@ import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
 	return (
-		<HashRouter basename="/">
+		<Router>
 			<Routes>
 				<Route exact path="/" element={<Home />} />
 				<Route exact path="/news" element={<News />} />
@@ -19,7 +18,7 @@ function App() {
 				<Route exact path="/expansion" element={<Expansion />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
-		</HashRouter>
+		</Router>
 	);
 }
 
